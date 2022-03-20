3e5fdf0cba53250344bae213b3a22411cd61d769
Sofiane AOUCI <touftoufeokami@gmail.com>
2022-03-20 02:30:05 +0100

    Created the view 	deleted:    software/lvgl/lvgl_f103/Core/Inc/Testboard_ui.h 	modified:   software/lvgl/lvgl_f103/Core/Inc/view.h 	deleted:    software/lvgl/lvgl_f103/Core/Src/Testboard_ui.c 	deleted:    software/lvgl/lvgl_f103/Core/Src/circle.c 	new file:   software/lvgl/lvgl_f103/Core/Src/circle90.c 	new file:   software/lvgl/lvgl_f103/Core/Src/font_cmd.c 	modified:   software/lvgl/lvgl_f103/Core/Src/main.c 	new file:   software/lvgl/lvgl_f103/Core/Src/view.c

--- software/lvgl/lvgl_f103/Core/Inc/view.h
+++ software/lvgl/lvgl_f103/Core/Inc/view.h
@@ -1,13 +1,27 @@
 /*
  * view.h
  *
- *  Created on: Jan 29, 2022
- *      Author: prince
+ *  Created on: Mar 20, 2022
+ *      Author: Sofiane
  */
 
 #ifndef INC_VIEW_H_
 #define INC_VIEW_H_
 
+#include "lvgl.h"
+
+typedef enum {
+	NONE = 0,
+	LEFT,
+	RIGHT,
+	BOTH
+}view_TL;
+
+void view_init();
+void view_setSpeed(int speed);
+void view_toggle_TL(view_TL direction);
+void view_setCMD(int cmd);
+void view_setLight(int turn_on);
 
 
 
